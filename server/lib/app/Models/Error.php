3b0f3021b4c59e1583f8ebb66e0d0c8b074e387d
Manuel Ottlik <manuel.ottlik@web.de>
2019-09-18 23:23:32 +0300

    generate server

---
+++ server/lib/app/Models/Error.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Error
+ */
+namespace app\Models;
+
+/**
+ * Error
+ */
+class Error {
+
+    /** @var int $code */
+    private $code;
+
+    /** @var string $error */
+    private $error;
+
+}
---
+++ server/lib/app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Task
+ */
+namespace app\Models;
+
+/**
+ * Task
+ */
+class Task {
+
+    /** @var int $id */
+    private $id;
+
+    /** @var string $name */
+    private $name;
+
+    /** @var bool $bookable */
+    private $bookable;
+
+    /** @var int $floorId */
+    private $floorId;
+
+    /** @var int $categoryId */
+    private $categoryId;
+
+}
